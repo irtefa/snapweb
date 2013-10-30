5ed3eb3acfb2094a2d1b1d837131fec06f0d3853
Mohd Irtefa <irtefa1@illinois.edu>
2013-10-30 02:39:21 -0500

    moved js code to inside event listener

--- js/camera.js
+++ js/camera.js
@@ -21,9 +21,8 @@ window.addEventListener("DOMContentLoaded", function() {
             video.play();
         }, errBack);
     }
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 480);
+    });
 }, false);
-
-// Trigger photo take
-document.getElementById("snap").addEventListener("click", function() {
-    context.drawImage(video, 0, 0, 640, 480);
-});
