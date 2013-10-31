760208f67bfa54fbd3c57cac9f86c36f9b6ac38d
Mohd Irtefa <irtefa1@illinois.edu>
2013-10-30 20:20:03 -0500

    added text with image

--- js/tools.js
+++ js/tools.js
@@ -2,4 +2,11 @@ $(document).ready( function(){
     $('li').click( function(){
         window.color = $(this).attr("id");
     });
+
+    $('#addText').click(function() {
+        var grabbedText = $('#textBox').val();
+        var context = document.getElementById('preview').getContext('2d');
+        context.fillText(grabbedText, 320, 240);
+    });
 });
+
