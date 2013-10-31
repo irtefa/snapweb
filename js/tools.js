b10b9d3e4280b7cd96eb2ab9a5ad4746a9c58aac
Mohd Irtefa <irtefa1@illinois.edu>
2013-10-30 20:22:55 -0500

    changed text style

--- js/tools.js
+++ js/tools.js
@@ -6,6 +6,8 @@ $(document).ready( function(){
     $('#addText').click(function() {
         var grabbedText = $('#textBox').val();
         var context = document.getElementById('preview').getContext('2d');
+        context.font = '40pt Calibri';
+        context.fillStyle = 'white';
         context.fillText(grabbedText, 320, 240);
     });
 });
