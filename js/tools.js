8bf611875ff525905a559df1bf83171457b2a4d6
Mohd Irtefa <irtefa1@illinois.edu>
2013-10-31 19:13:21 -0500

    text now working with new style

--- js/tools.js
+++ js/tools.js
@@ -5,7 +5,7 @@ $(document).ready( function(){
 
     $('#addText').click(function() {
         var grabbedText = $('#textBox').val();
-        var context = document.getElementById('preview').getContext('2d');
+        var context = document.getElementById('hiddenCanvas').getContext('2d');
         context.font = '40pt Calibri';
         context.fillStyle = 'white';
         context.fillText(grabbedText, 320, 240);
